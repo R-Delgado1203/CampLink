67178a4aa1f117ba47edf9c0c0867ae1808e77d4
RolandoD <r.delgado1203@gmail.com>
2018-11-18 21:14:40 -0700

    updated html routes and api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,55 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+module.exports = function (app) {
+  // find all users
+  app.get("/api/users", function (req, res) {
+    db.User.findAll({})
+      .then(function (dbUsers) {
+        res.json(dbUsers);
+      });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //search via first name
+  app.get("/api/search/user/:firstName", function (req, res) {
+    db.User.findAll({
+      where: {
+        first_name: req.params.firstName
+      }
+    })
+      .then(function (dbUser) {
+        res.json(dbUser);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //search via last name
+  app.get("/api/search/user/:lastName", function (req, res) {
+    db.User.findAll({
+      where: {
+        last_name: req.params.lastName
+      }
+    })
+      .then(function (dbUser) {
+        res.json(dbUser);
+      });
+  });
+
+  //search via year
+  app.get("/api/search/user/:year", function (req, res) {
+    db.User.findAll({
+      where: {
+        bootcamp_year: req.params.year
+      }
+    })
+      .then(function (dbUser) {
+        res.json(dbUser);
+      });
+  });
+
+  //add new user
+  app.post("/api/signup", function (req, res) {
+    db.User.create(req.body)
+      .then(function (dbUser) {
+        res.json(dbUser);
+      });
   });
 };
