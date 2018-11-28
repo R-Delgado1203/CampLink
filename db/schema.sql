daf91cd57d97e4e407641549bc1210d213af21c7
Peyton <41251008+pxgraham@users.noreply.github.com>
2018-11-27 19:04:14 -0700

    added cohort page to html routes and views. also added charlies updated html to signup.handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,2 @@
 DROP DATABASE IF EXISTS alumni_db;
 CREATE DATABASE alumni_db;
-
-USE alumni_db;
-
-CREATE TABLE signup_info (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    bootcamp_year VARCHAR(25),
-    email VARCHAR(100) NOT NULL,
-    p_password VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE profile_info (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    profile_pic VARCHAR(100),
-    github VARCHAR(100),
-    portfolio VARCHAR(100), 
-    favorite_language VARCHAR(50),
-    current_job VARCHAR(50),
-    hiring BOOLEAN,
-    job_searching BOOLEAN,
-    PRIMARY KEY (id)
-);
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var path = require("path");
+var db = require("../models");
 
 module.exports = function(app) {
 
@@ -12,6 +13,17 @@ module.exports = function(app) {
 
   app.get("/profile", function(req, res) {
     res.render(path.join(__dirname, "../views/profile.handlebars"));
+  });
+
+  //page to display all profiles
+  app.get("/cohort", function (req, res) {
+    db.user.findAll({})
+      .then(function (data) {
+        var userData = {
+          users: data
+        }
+        res.render('cohort', userData);
+      });
   });
 
   // Render 404 page for any unmatched routes
